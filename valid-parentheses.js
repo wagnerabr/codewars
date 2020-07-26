12484c272d32082140d1dbd3b5a6f06989c2d9f2
Wagner Barros <wagner.abr@hotmail.com>
2020-07-25 23:48:14 -0300

    Create function valid parentheses

--- valid-parentheses.js
+++ valid-parentheses.js
@@ -1,15 +1,53 @@
 var Test = require('assert');
 
-const validParentheses = (parens) => {
-    console.log(parens.length);
-    return (parens !== null)? (parens.length > 1 && parens.length <= 100)? (parens.match(/\(/g).length === parens.match(/\)/g).length)? true : false : false : false;
+const validParentheses2 = (parens = null) => {
+    return (parens !== null)? (parens.length > 1 && parens.length <= 100)? (parens.match(/\(/g).length === parens.match(/\)/g).length)? (parens.indexOf("(") < parens.indexOf(")"))? true : false : false : false : false;
 }
 
-// Test.equal(validParentheses(null), false);
-// Test.equal(validParentheses( "()" ), true);
-// Test.equal(validParentheses( "())" ), false);
-// Test.equal(validParentheses( ")(()))" ), false);
-// Test.equal(validParentheses( "(" ), false);
-// Test.equal(validParentheses( "(())((()())())" ), true);
+function validParentheses3(parens){
+    var n = 0;
+    for (var i = 0; i < parens.length; i++) {
+      if (parens[i] == '(') n++;
+      if (parens[i] == ')') n--;
+      if (n < 0) return false;
+    }
+    
+    return n == 0;
+  }
 
+var validParentheses = function (s) {
+    if(s === null) return false
+    const obj = {
+      "(": ")",
+      "{": "}",
+      "[": "]",
+    }
+  
+    const stack = [];
+
+    for (const paran of s) {
+      if (obj.hasOwnProperty(paran)) {
+          //console.log(paran)
+        stack.push(paran)
+      } else {
+        const closeParan = stack.pop();
+        console.log(closeParan);
+        if (paran !== obj[closeParan]) {
+          return false;
+        }
+      }
+      //console.log(stack);
+    }
+  
+    return stack.length === 0;
+  };
+
+Test.equal(validParentheses(null), false);
+Test.equal(validParentheses( ")(" ), false);
+Test.equal(validParentheses( "()" ), true);
+Test.equal(validParentheses( "())" ), false);
+Test.equal(validParentheses( ")(()))" ), false);
+Test.equal(validParentheses( "(" ), false);
+Test.equal(validParentheses( ")" ), false);
+Test.equal(validParentheses( "(())" ), true);
 Test.equal(validParentheses( "(())((()())())(())((()())())(())((()())())(())((()())())(())((()())())(())((()())())(())((()())())()" ), true);
